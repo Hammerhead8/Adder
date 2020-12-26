57e8acac6852afc25e4b9736562edcc23d889e17
Joshua Cates <hammerhead810@gmail.com>
2020-12-25 22:00:28 -0500

    :construction: Added QR factorization

--- linalg.h
+++ linalg.h
@@ -11,6 +11,7 @@ Matrix * transpose (Matrix *m);
 Matrix * inverse (Matrix *m);
 Vector * linearSolve (Matrix *M, Vector *b);
 Vector * eigenValues (Matrix *m);
+Matrix * qr (Matrix *m);
 Matrix * lq (Matrix *m);
 Matrix * lu (Matrix *m);
 void svd (Matrix *m);
