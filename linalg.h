8297c60368a000cab966f16fc83025bd03bbf3f1
Joshua Cates <hammerhead810@gmail.com>
2021-08-02 18:41:08 -0400

    :triangular_ruler: Sectioned the functions

--- linalg.h
+++ linalg.h
@@ -7,14 +7,25 @@
 
 #include "matrix.h"
 
+/* Extra matrix functions */
 Matrix * transpose (Matrix *m);
 Matrix * inverse (Matrix *m);
+
+/* Equation solving */
 Vector * linearSolve (Matrix *M, Vector *b);
+
+/* Eigenvalues */
 Vector * eigenValues (Matrix *m);
+
+/* Matrix factorizations */
 Matrix * qr (Matrix *m);
 Matrix * lq (Matrix *m);
 Matrix * lu (Matrix *m);
+
+/* Singular value decomposition */
 void svd (Matrix *m);
+
+/* Norms */
 double vectorNorm (Vector *v);
 double matrixNorm (Matrix *m);
 void vectorTranspose (Vector *v);
