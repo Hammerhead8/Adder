3235c4c19bb9eca7caef827fa31f8546152a7a77
Joshua Cates <hammerhead810@gmail.com>
2021-12-12 18:08:30 -0500

    :bug: Fixed new fraction not being a pointer in function newFraction

--- fractions.c
+++ fractions.c
@@ -1,15 +1,22 @@
 /* fractions.c
  * Function definitions for the prototypes in fractions.h */
-#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h> /* For malloc and free */
+#include <stddef.h>
+#include <string.h> /* For strlen */
 #include <math.h> /* For pow */
 #include "fractions.h"
+
+/******************
+ * Real fractions *
+ ******************/
 
 /* IO functions */
 /* Allocate the memory for a new fraction */
 adder_fraction *
 newFraction (long int numerator, long int denominator)
 {
-	adder_fraction f;
+	adder_fraction *f;
 	f = malloc (sizeof (adder_fraction));
 	if (f == NULL) {
 		return f;
@@ -26,16 +33,179 @@ newFraction (long int numerator, long int denominator)
 	}
 }
 
+/* Create a fraction from a decimal number given as a string */
+adder_fraction *
+newFractionFromString (unsigned char *num)
+{
+	adder_fraction *f;
+	long int fracNum = 0;
+	long int intNum = 0;
+	long unsigned int len;
+	long unsigned int index;
+	unsigned char *indexPtr;
+	long unsigned int i;
+
+	f = malloc (sizeof (adder_fraction));
+	if (f == NULL) {
+		fprintf (stderr, "Failed to create fraction.\n");
+		return NULL;
+	}
+
+	/* Convert from decimal to fraction */
+	/* Start by calculating the length of the string */
+	len = strlen (num);
+
+	/* Then find the index of the decimal point in the string */
+	indexPtr = strchr (num, 0x2e);
+
+	/* A lot of boring string parsing will follow */
+
+	/* If indexPtr is NULL then the number is an integer */
+	if (indexPtr == 0x00) {
+		/* If the first character is a '-' then the number is negative */
+		if (num[0] == 0x5f) {
+			f->denom = 1;
+
+			/* Loop through the digits to combine them into one number */
+			for (i = len - 1; i >= 0; i--) {
+				/* Check that the digit is valid */
+				if (num[i] < 0x30 || num[i] > 0x39) {
+					fprintf (stderr, "Invalid character found in digit string.\n");
+					free (f);
+					return NULL;
+				}
+
+				/* Convert the digit from ASCII to decimal */
+				num[i] -= 0x30;
+				f->numer += (long int)num[i] * pow (10, (len - 1) - i);
+			}
+
+			f->numer *= -1;
+		}
+
+		/* Otherwise the number is positive */
+		else {
+			f->denom = 1;
+
+			/* Loop through the digits and combine them */
+			for (i = len - 1; i >= 0; i--) {
+				/* Check that the digit is valid */
+				if (num[i] < 0x30 || num[i] > 0x30) {
+					fprintf (stderr, "Invalid character in decimal string.\n");
+					free (f);
+					return NULL;
+				}
+
+				/* Convert each digit from ASCII to decimal */
+				num[i] -= 0x30;
+				f->numer += (long int)num[i] * pow (10, (len - 1) - i);
+			}
+		}
+	}
+
+	/* Otherwise the number is a floating point number */
+	else {
+		/* Get the location of the decimal point
+		 * indexPtr is a pointer (holds the address to)
+		 * the decimal point and number is a pointer
+		 * to the leading digit in the number.
+		 * Subtracting them will result in the
+		 * index in the array where the decimal point is. */
+		index = (long unsigned int)(indexPtr - num);
+
+		/* Calculate the number of digits in the number */
+		len = strlen (num);
+
+		/* If a '-' is the first character then the number is negative */
+		if (num[0] == 0x5f) {
+			/* First loop through the decimal part of the number */
+			for (i = index + 1; i < len; i++) {
+				/* Check if the digit is valid */
+				if (num[i] < 0x30 || num[i] > 0x39) {
+					fprintf (stderr, "Invalid character in fraction string.\n");
+					free (f);
+					return NULL;
+				}
+
+				/* Convert the digit from ASCII to decimal */
+				num[i] -= 0x30;
+				fracNum += (long int)num[i] * pow (10, (index - 1) - i);
+			}
+
+			f->denom = pow (10, index - 1);
+
+			/* If there is either no digit or a zero before the decimal point then
+			 * skip this part. Otherwise we need to deal with the integer part. */
+			if (num[index - 1] != 0x30 && num[index - 1] != 0x2e) {
+				for (i = index - 1; i >= 1; i--) {
+					/* Check if the digit is a valid character */
+					if (num[i] < 0x30 || num[i] > 0x39) {
+						fprintf (stderr, "Invalid character in the fraction string.\n");
+						free (f);
+						return NULL;
+					}
+
+					/* Convert the digit from ASCII to decmial */
+					num[i] -= 0x30;
+					intNum += (long int)num[i] * pow (10, (index - 1) - i) * f->denom;
+				}
+			}
+		}
+
+		/* Otherwise the number is positive */
+		else {
+			/* First loop through the decimal part of the number */
+			for (i = index + 1; i < len; i++) {
+				/* Check if the digit is valid */
+				if (num[i] < 0x30 || num[i] > 0x39) {
+					fprintf (stderr, "Invalid character in fraction string.\n");
+					free (f);
+					return NULL;
+				}
+
+				/* Convert the digit from ASCII to decimal */
+				num[i] -= 0x30;
+				fracNum += (long int)num[i] * pow (10, (index - 1) - i);
+			}
+
+			f->denom = pow (10, index - 1);
+
+			/* If there is either no digit or a zero before the decimal point then
+			 * skip this part. Otherwise we need to deal with the integer part. */
+			if (num[index - 1] != 0x30 && num[index - 1] != 0x2e) {
+				for (i = index - 1; i >= 1; i--) {
+					/* Check if the digit is a valid character */
+					if (num[i] < 0x30 || num[i] > 0x39) {
+						fprintf (stderr, "Invalid character in the fraction string.\n");
+						free (f);
+						return NULL;
+					}
+
+					/* Convert the digit from ASCII to decimal */
+					num[i] -= 0x30;
+					intNum += (long int)num[i] * pow (10, (index - 1) - i) * f->denom;
+				}
+			}
+		}
+	}
+
+	/* The last step is to add fracNum and intNum to find the total numerator */
+	f->numer = fracNum + intNum;
+
+	/* The fraction should be fully calculated */
+	return f;
+}
+
 /* Deallocate the memory for the fraction */
 void
-deleteadder_fraction (adder_fraction *f)
+deleteFraction (adder_fraction *f)
 {
 	free (f);
 }
 
 /* Print the fraction */
 void
-print (adder_fraction *f)
+printFraction (adder_fraction *f)
 {
 	printf ("%ld/%ld\n", f->numer, f->denom);
 }
@@ -123,8 +293,8 @@ subFractions (adder_fraction *diff, adder_fraction *f1, adder_fraction *f2)
 	numerator2 = f2->numer * N2;
 
 	/* Perform the subtraction */
-	sum->numer = numerator1 - numerator2;
-	sum->denom = lcm;
+	diff->numer = numerator1 - numerator2;
+	diff->denom = lcm;
 }
 
 /* Multiply two fractions */
@@ -162,14 +332,19 @@ power (adder_fraction *f, long int exponent)
 	f->denom = pow (f->denom, (double)exponent);
 }
 
+
 /* Other functions */
 /* Simplify the fraction */
 void
 simplify (adder_fraction *f)
 {
 	long int gcd;
+	
+	if (f->numer == 0 || f->numer == 1) {
+		return;
+	}
 
-	if (f->numer < 0 && f->denom > 0) {
+	else if (f->numer < 0 && f->denom > 0) {
 		gcd = calcGCD (-1 * f->numer, f->denom);
 	}
 	else {
@@ -184,6 +359,7 @@ simplify (adder_fraction *f)
 		f->denom /= gcd;
 	}
 }
+
 /* Calculate the GCD using Euler's Method */
 long int
 calcGCD (long int a, long int b)
