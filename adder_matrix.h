5c119dbd6209554076c38c6c46262bab43b5ec5b
Joshua Cates <hammerhead810@gmail.com>
2022-01-13 18:03:44 -0500

    Matrices are now only stored in row major

--- adder_matrix.h
+++ adder_matrix.h
@@ -21,12 +21,12 @@ VECTOR_ORDER
 	COLUMN_VECTOR = 1
 };
 
-enum
+/*enum
 MATRIX_ORIENTATION
 {
 	ROW_MAJOR = 1,
 	COLUMN_MAJOR = 2
-};
+}; */
 
 /* Vector type definition */
 typedef struct
@@ -50,7 +50,6 @@ typedef struct
 	double *mat;
 	int rows;
 	int columns;
-	int orientation;
 } adder_matrix;
 
 /* Complex matrix type definition */
@@ -59,7 +58,6 @@ typedef struct
     adder_complex_rect *mat;
     int rows;
     int columns;
-    int orientation;
 } adder_complex_matrix;
 
 /* Vector IO functions */
@@ -84,20 +82,22 @@ void vectorTranspose (adder_vector *v);
 void complexVectorTranspose (adder_complex_vector *z);
 
 /* Matrix IO functions */
-adder_matrix * matrixInit (int orient, int numRows, int numColumns, double *values);
-adder_matrix *matrixInit2 (int orient, int numRows, int numcolumns);
+adder_matrix * matrixInit (int numRows, int numColumns, double *values);
+adder_matrix *matrixInit2 (int numRows, int numcolumns);
 void deleteMatrix (adder_matrix *m);
 void printMatrix (adder_matrix *m);
 
 /* Complex matrix IO functions */
-adder_complex_matrix * complexMatrixInit (int orient, int numRows, int numColumns, double *realValues, double *imagValues);
-adder_complex_matrix * complexMatrixInit2 (int orient, int numRows, int numColumns);
+adder_complex_matrix * complexMatrixInit (int numRows, int numColumns, double *realValues, double *imagValues);
+adder_complex_matrix * complexMatrixInit2 (int numRows, int numColumns);
 void deleteComplexMatrix (adder_complex_matrix *Z);
 void printComplexMatrix (adder_complex_matrix *Z);
 
 /* Matrix arithmetic functions */
 adder_vector * mvMultiply (adder_matrix *M, adder_vector *v);
 adder_matrix * mmMultiply (adder_matrix *A, adder_matrix *B);
+adder_complex_vector * mvMultiplyComplex (adder_complex_matrix *Z, adder_complex_vector *v);
+adder_complex_matrix * mmMultiplyComplex (adder_complex_matrix *Y, adder_complex_matrix *Z);
 
 /* Matrix setting functions */
 void eye (adder_matrix *m, long int n);
