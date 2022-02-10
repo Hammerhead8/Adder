a484f9e2d7f83f4e0d892e62895c500c568e7029
Joshua Cates <hammerhead810@gmail.com>
2022-02-10 00:44:44 -0500

    :wrench: Fixed interface for trapezoidal and Simpson's rule methods

--- adder_integration.h
+++ adder_integration.h
@@ -3,10 +3,10 @@
  * Integration can be done using Gauss-Legendre quadrature or
  * the Trapezoidal rule.
  * All integration routines use double precision */
-#ifndef ADDER_INTEGRATION_H
-#define ADDER_INTEGRATION_H
+#ifndef INTEGRATION_H
+#define INTEGRATION_H
 
-#include "adder_math.h" /* For adder_function */
+#include "../adder_math.h" /* For adder_function */
 
 enum
 IntegralErrors
@@ -21,10 +21,13 @@ double gauss5 (adder_function *f, double a, double b);
 double gauss20 (adder_function *f, double a, double b);
 double gauss50 (adder_function *f, double a, double b);
 double gauss100 (adder_function *f, double a, double b);
-double gaussQuad (adder_function *f, double a, double b, double *xi, double *wi, int N);
+//double gaussQuad (adder_function *f, double *res, double a, double b, int N);
+int galeQuad (adder_function *f, double *res, double a, double b, int N);
 
-/* Other integration routines */
-double trapezoidIntegrate (adder_function *f, double a, double b, double *points, int N);
-double simpsonIntegrate (adder_function *f, double a, double b, double *points, int N);
+/*Other integration routines */
+double trapezoidIntegrate (adder_function *f, double a, double b, int N);
+double simpsonIntegrate (adder_function *f, double a, double b, int N);
+double monteCarloIntegrate (adder_function *f, double a, double b, long unsigned int N);
+//double mdMonteCarloIntegration (adder_muldim_function *f, double *a, double *b, long unsigned int dims, long unsigned int N);
 
 #endif
