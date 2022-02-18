ee47a13969c937186578c8afac6e27bf45f64290
Joshua Cates <hammerhead810@gmail.com>
2022-02-18 15:23:12 -0500

    Removed QR, LQ, and LU factorizations

--- adder_linalg.h
+++ adder_linalg.h
@@ -21,11 +21,6 @@ adder_vector * linearLeastSquares (adder_matrix *M, adder_vector *b);
 adder_vector * eigenValues (adder_matrix *m);
 adder_complex_vector * complexEigenValues (adder_complex_matrix *m);
 
-/* Matrix factorizations */
-adder_matrix * qr (adder_matrix *m);
-adder_matrix * lq (adder_matrix *m);
-adder_matrix * lu (adder_matrix *m);
-
 /* Singular value decomposition */
 adder_vector * svd (adder_matrix *m);
 
