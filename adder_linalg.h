bc7a38f9d0d15655c4eb7e941ae6b8f0ea76dc4a
Joshua Cates <hammerhead810@gmail.com>
2022-11-29 09:42:18 -0500

    :sparkles: Implemented exponetial curve fitting using linear least squares

--- adder_linalg.h
+++ adder_linalg.h
@@ -2,34 +2,28 @@
  *
  * This serves as an extension to matrix.h and includes linear algebra
  * routines for matrices and vectors using OpenBLAS and LAPACKE */
-#ifndef LINALG_H
-#define LINALG_H
+#ifndef ADDER_LINALG_H
+#define ADDER_LINALG_H
 
 #include "adder_matrix.h"
 
-/*TODO:  Implement complex pseudoinverse */
 /* Extra matrix functions */
 adder_matrix * inverse (adder_matrix *m);
 adder_matrix * pseudoinverse (adder_matrix *m);
-adder_complex_matrix * complexInverse (adder_complex_matrix *m);
-adder_complex_matrix * complexPseudoinverse (adder_complex_matrix *m);
 
 /* Equation solving */
 adder_vector * linearSolve (adder_matrix *M, adder_vector *b);
 adder_vector * odLinearSolve (adder_matrix *M, adder_vector *b);
 adder_vector * linearLeastSquares (adder_matrix *M, adder_vector *b);
-adder_complex_vector * complexLinearSolve (adder_complex_matrix *M, adder_complex_vector *b);
 
-/* Curve fitting routines */
+/* Curve fitting functions */
 adder_vector * exponentialFit (adder_matrix *M, adder_vector *b);
 
 /* Eigenvalues */
 adder_vector * eigenValues (adder_matrix *m);
-adder_complex_vector * complexEigenValues (adder_complex_matrix *m);
 
 /* Singular value decomposition */
 adder_vector * svd (adder_matrix *m);
-adder_complex_vector * complexSVD (adder_complex_matrix *m);
 
 /* Norms */
 double vectorNorm (adder_vector *v);
